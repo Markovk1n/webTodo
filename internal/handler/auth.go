fb1c30a4d723fa0c69db23406893020a7ecc5516
pc <roalk733@gmail.com>
2024-04-18 11:40:26 +0600

    added | log-in service | JWT token

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -24,9 +24,30 @@ func (h *Handler) singUp(c *gin.Context) {
 		"id": id,
 	})
 
-	c.JSON(http.StatusOK, "Sing-up")
+}
+
+type singInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 func (h *Handler) singIn(c *gin.Context) {
-	c.JSON(http.StatusOK, "Sing-in")
+	var input singInInput
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, "ahshdauhdiuahdiushaidhaishdsaiuhdiuahdia")
+		return
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+
+		return
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
+
 }
